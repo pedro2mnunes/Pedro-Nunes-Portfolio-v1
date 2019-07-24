617f47cd9eb363c8ffe632af7c3537aea433d1f6
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2019-07-24 23:19:07 +0100

    Insert link to parallax.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 var $header = $('header');
 var $sticky = $header.before($header.clone().addClass("sticky"));
+$(window).on("scroll",function(){
+var scrollFromTop =$(window).scrollTop(); /* scrollTop() is a jQuery method*/
+$("body").toggleClass("scroll",(scrollFromTop > 350));
+});
+
 });
 
