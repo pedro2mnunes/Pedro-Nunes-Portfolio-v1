3e7b94b0f139f3d1327e55196b1cb4e818eeb199
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2019-07-27 10:27:13 +0100

    Add Responsive Menu Start.

--- scripts/main.js
+++ scripts/main.js
@@ -18,5 +18,23 @@ $("body").toggleClass("scroll",(scrollFromTop > 350));
     }
 
 });
+ //RESPONSIVE MENU START
+
+ var body = $('body');
+ var menuTrigger = $('.js-menu-trigger');
+ var mainOverlay = $('.js-main-overlay');
+
+ menuTrigger.on('click', function(){
+   body.addClass('menu-is-active');
+ });
+
+ mainOverlay.on('click', function(){
+   body.removeClass('menu-is-active');
+ });
+
+ $('.menu li a').on('click', function(){
+     $('body').removeClass("menu-is-active");
+ });
+
 });
 
