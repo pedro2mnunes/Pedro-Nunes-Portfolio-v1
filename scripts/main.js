ccb5e5de7875c9d1d1227d5c237d2458a2b327fa
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2019-07-27 10:51:52 +0100

    Add Slick Slider.

--- scripts/main.js
+++ scripts/main.js
@@ -18,6 +18,34 @@ $("body").toggleClass("scroll",(scrollFromTop > 350));
     }
 
 });
+//SLICK SLIDER
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed:1500,
+    arrows: true,
+    dots: false,
+    centerMode: true,
+    slidesToShow: 3,
+    fade:false,
+    prevArrow:'<button type="button" class="slick-prev">Previous</button>',
+    nextArrow:'<button type="button" class="slick-next">Previous</button>',
+
+    responsive: [
+      {
+          breakpoint: 990,
+          settings: {
+              slidesToShow: 2
+          }
+      },
+      {
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 1
+          }
+      }
+    ]
+});
+
  //RESPONSIVE MENU START
 
  var body = $('body');
